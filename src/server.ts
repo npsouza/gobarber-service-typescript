46c47aaeefe0394efd053be349f7d5e41bf117f8
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-01-14 19:55:29 -0300

    configurando estrutura de pastas, adicionando debugger

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.post('/users', (request, response) => {
+  const {name, email} = request.body;
+
+  const user = {
+    name, 
+    email
+  }
+
+  return response.json(user)
+})
+
+app.listen(3333, () => {
+  console.log('Server started')
+})
