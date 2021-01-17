f8552e8dc527b6c0917065252ed6ad0b99f81270
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-01-17 12:51:44 -0300

    controlar autenticação, criando middlewares

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '6a204bd89f3c8348afd5c77c717a097a',
+    expiresIn: '1d',
+  },
+};
--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -2,6 +2,7 @@ import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 
+import authConfig from '../config/auth';
 import User from '../models/User';
 
 interface Request {
@@ -30,9 +31,11 @@ class AuthenticateUserService {
       throw new Error('Incorret email/password combination');
     }
 
-    const token = sign({}, '6a204bd89f3c8348afd5c77c717a097a', {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
