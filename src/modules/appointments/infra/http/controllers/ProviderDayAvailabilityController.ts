a97935ffeeb9c72610d1231207595fabccba27c8
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-03-31 12:39:08 -0300

    criando controller e rotas para listar disponibilidade

---
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,23 @@
+import ListProvidersDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+export default class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { month, year, day } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProvidersDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      month,
+      year,
+      day,
+    });
+
+    return response.json(availability);
+  }
+}
