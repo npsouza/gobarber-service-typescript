9a955126e1b4915cbb16446d9c32f9bdb6c286b2
Nathan Pereira <nathanpereirasouza@gmail.com>
2021-03-31 11:36:53 -0300

    fix teste reset password service

--- src/modules/users/services/ResetPasswordService.spec.ts
+++ src/modules/users/services/ResetPasswordService.spec.ts
@@ -76,7 +76,7 @@ describe('ResetPassword', () => {
 
     const { token } = await fakeUserTokensRepository.generate(user.id);
 
-    jest.spyOn(Date, 'now').mockImplementation(() => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
       const customDate = new Date();
 
       return customDate.setHours(customDate.getHours() + 3);
