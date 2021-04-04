9c3686e2291a69d65394bdd7f2fb6feacd642b86
Nathan Souza <nathanpereirasouza@gmail.com>
2021-04-04 19:39:30 -0300

    utilizando query params

--- src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -5,7 +5,7 @@ import { container } from 'tsyringe';
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { month, year, day } = request.body;
+    const { month, year, day } = request.query;
 
     const listProviderDayAvailability = container.resolve(
       ListProvidersDayAvailabilityService,
@@ -13,9 +13,9 @@ export default class ProviderDayAvailabilityController {
 
     const availability = await listProviderDayAvailability.execute({
       provider_id,
-      month,
-      year,
-      day,
+      month: Number(month),
+      year: Number(year),
+      day: Number(day),
     });
 
     return response.json(availability);
