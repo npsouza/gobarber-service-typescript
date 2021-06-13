92c5a65c8088e4c2faf334ff0f0e625873f8e137
Nathan Souza <nathanpereirasouza@gmail.com>
2021-06-13 18:57:51 -0300

    adicionando serialização do providers com classToClass

--- src/modules/appointments/infra/http/controllers/ProvidersController.ts
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -1,6 +1,8 @@
-import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
 
 export default class ProvidersController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -11,6 +13,6 @@ export default class ProvidersController {
       user_id,
     });
 
-    return response.json(providers);
+    return response.json(classToClass(providers));
   }
 }
